db47974128b86a9d85273a78c7a7ed4ccf05e6de
Erudit Morina <erudit.morina@gmail.com>
2023-08-16 22:09:21 +0200

    provide code documentation

--- lib/audit.module.ts
+++ lib/audit.module.ts
@@ -2,11 +2,22 @@ import { DynamicModule, Module, Provider } from '@nestjs/common';
 import { AuditService } from './audit.service';
 import { AuditConfig } from './interfaces';
 
+/**
+ * Audit module class that provides audit service capabilities.
+ *
+ */
 @Module({
   providers: [AuditService],
   exports: [AuditService],
 })
 export class AuditModule {
+  /**
+   * Provide audit service with options set at module level.
+   *
+   * @param config transport name and options
+   * @returns `ModuleMetadata` for Audit module
+   *
+   */
   static forRoot(config: AuditConfig): DynamicModule {
     const provider = AuditModule.createProvider(config);
     return {
--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,8 @@
+/*
+ * nestjs-audit
+ * Copyright(c) 2023 Erudit Morina
+ * MIT Licensed
+ */
 export * from './audit.module';
 export * from './audit.service';
 export * from './decorators';
