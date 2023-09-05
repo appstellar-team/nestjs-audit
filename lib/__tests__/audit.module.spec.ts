f15e9d6cbb476704e9246da4ef1db583b245a83a
Erudit Morina <erudit.morina@gmail.com>
2023-09-05 15:55:06 +0200

    test audit service

--- lib/__tests__/audit.module.spec.ts
+++ lib/__tests__/audit.module.spec.ts
@@ -2,8 +2,13 @@ import { Test } from '@nestjs/testing';
 import { AuditModule } from '../audit.module';
 import { AuditService } from '../audit.service';
 import { TransportMethods } from '../interfaces';
+import { faker } from '@faker-js/faker';
 
 describe('Audit Module', () => {
+  afterAll(() => {
+    jest.resetModules();
+  });
+
   it('should import audit module succesfully', async () => {
     const module = await Test.createTestingModule({
       imports: [AuditModule],
@@ -20,7 +25,7 @@ describe('Audit Module', () => {
       imports: [
         AuditModule.forRoot({
           transports: [{ name: TransportMethods.CONSOLE }],
-          getUserId: () => 'user-id',
+          getUserId: () => faker.string.uuid(),
           logErrors: true,
         }),
       ],
---
+++ lib/__tests__/audit.service.spec.ts
@@ -0,0 +1,92 @@
+import { faker } from '@faker-js/faker';
+import { AuditService } from '../audit.service';
+import {
+  Action,
+  AuditLogger,
+  AuditParams,
+  Outcome,
+  TransportMethods,
+} from '../interfaces';
+import { Logger } from '@nestjs/common';
+
+describe('Audit Service', () => {
+  let auditService: AuditService;
+
+  const mockAuditParams: AuditParams = {
+    action: Action.READ,
+    getUserId: () => faker.string.uuid(),
+    getResponseObjectId: () => faker.string.uuid(),
+    entity: 'users',
+  };
+
+  const mockAuditLogger: AuditLogger = {
+    params: mockAuditParams,
+    outcome: Outcome.SUCCESS,
+  };
+
+  beforeEach(() => {
+    auditService = new AuditService();
+  });
+
+  it('should set logErrors to true', () => {
+    auditService.logErrors = true;
+    expect(auditService.logErrors).toBeTruthy();
+  });
+
+  it('should return value of action', () => {
+    expect(auditService.getAction()).toBeUndefined();
+  });
+
+  it('should set a value to action', () => {
+    auditService.setAction(Action.READ);
+    expect(auditService.getAction()).toBeDefined();
+    expect(auditService.getAction()).toBe(Action.READ);
+  });
+
+  it('should return user id callback', () => {
+    expect(auditService.getUserIdCallback()).toBeUndefined();
+  });
+
+  it('should set user id callback', () => {
+    auditService.setUserIdCallback(() => faker.string.uuid());
+    expect(auditService.getUserIdCallback()).toBeDefined();
+  });
+
+  it('should return response object id callback', () => {
+    expect(auditService.getResponseObjectIdCallback()).toBeUndefined();
+  });
+
+  it('should set response object id callback', () => {
+    auditService.setResponseObjectIdCallback(() => faker.string.uuid());
+    expect(auditService.getResponseObjectIdCallback()).toBeDefined();
+  });
+
+  it('should return name of entity changed', () => {
+    expect(auditService.getEntityName()).toBeUndefined();
+  });
+
+  it('should set the name of entity', () => {
+    const entityName = 'users';
+    auditService.setEntityName(entityName);
+    expect(auditService.getEntityName()).toBeDefined();
+    expect(auditService.getEntityName()).toBe(entityName);
+  });
+
+  it('should return list of transport methods', () => {
+    expect(auditService.getTransports().length).toBe(0);
+  });
+
+  it('should add transport to list of transports successfully', async () => {
+    auditService.addTransport(TransportMethods.CONSOLE);
+    expect(auditService.getTransports().length).toBeGreaterThan(0);
+  });
+
+  it('should keep audit logs', async () => {
+    const loggerMock = jest.spyOn(Logger, 'log');
+    await auditService.log(mockAuditLogger, { method: 'GET' });
+
+    expect(auditService.getAction()).toBe(Action.READ);
+    expect(auditService.getTransports().length).toBeGreaterThan(0);
+    expect(loggerMock).toHaveBeenCalledWith('Auditing complete!');
+  });
+});
